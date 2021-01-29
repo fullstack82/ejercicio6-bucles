b855c40bd857216d2a8bf2bb64700bb4eb425097
fullstack82 <fullstack8210@gmail.com>
2021-01-29 13:59:51 +0100

    calculadora terminada

--- index.php
+++ index.php
@@ -2,20 +2,37 @@
 /* Mostrar una tabla de html con las tablas de multiplicar del 1 al 10;
 */
 
-
 echo "<table border='1'> <tr>";  // Inicio de la tabla
 
-echo "<tr>";  // Inicio fila 1 de celdas
 
+
+echo "<tr>";  // Inicio fila 1 de celdas
 for ($cabecera = 1; $cabecera <= 10; $cabecera++) {
 
     echo "<td>Tabla del $cabecera</td>";
 }
+echo "</tr>";  // Cierre de la fila 1 de celdas
+
+
+
+echo "<tr>";   // Inicio de la 2 fila de celdas
+
+for($i = 1; $i <= 10; $i++){
+    echo "<td>";
+
+        for($x = 1; $x <= 10; $x++){
+            echo "$i x $x = ".($i*$x)."<br/>";
+        }
 
 
 
 
 
-echo "</tr>";  // Cierre de celdas
+    echo "</td>";
+}
+
+
+
+echo "</tr>"; // Cierre de la fila 2 de celdas
 
 echo "</table>";   // Fin de la tabla
